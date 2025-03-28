3cf298ab3a5ad380aa1d5c7176d52a0b3dcaabf4
JKaypa <jose_kaypa@hotmail.com>
2025-03-27 21:15:10 -0500

    add middleware

--- src/common/square-root/square-root.route.ts
+++ src/common/square-root/square-root.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import {SquareRootController} from "@/common/square-root/square-root.controller.class";
+import { SquareRootController } from "@/common/square-root/square-root.controller.class";
+import { validateRequest } from "@/common/middleware/validationRequest";
+
 
 const squareRoot = Router();
 
-squareRoot.post('/calculate', SquareRootController.calculation)
+squareRoot.post('/calculate', validateRequest, SquareRootController.calculation)
 
 export default squareRoot
